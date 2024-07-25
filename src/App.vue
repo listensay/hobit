5e6eb029b6427db04ca9f64617908a13b508796d
Miliya <odr233@gmail.com>
2024-07-25 14:56:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div>
+      今天
+      <div>
+        <input type="text" placeholder="添加一个任务, 按回车键保存" />
+        <MazBtn>Button</MazBtn>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+// Maz-ui
+import 'maz-ui/styles'
+
+
+createApp(App).mount("#app");
